5c1b35cb3471f43170d3bce6c2448c8e9f1cfffa
ronperris <ronperris@gmail.com>
2015-10-18 11:44:06 -0700

    Add password length check to /join/ route. #1
    The password must be at least 6 characters or the user will not be added.

--- server/routes/join.js
+++ server/routes/join.js
@@ -2,11 +2,14 @@ var express = require('express');
 var app = express();
 var join = express.Router();
 
-join.get('/:name/:password', function(req, res){
-  if(req.params.name.length < 3) {
-    res.sendStatus(404);
-  } else {
+join.get('/:name/:secret', function(req, res){
+  var name = req.params.name;
+  var secret = req.params.secret;
+
+  if(name.length > 3 && secret.length > 5) {
     res.send('joined')
+  } else {
+    res.sendStatus(404);
   }  
 });
 
