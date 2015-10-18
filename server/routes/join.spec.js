5627794d3f9731c50442e6bd37be8b83dadc9461
ronperris <ronperris@gmail.com>
2015-10-18 16:39:15 -0700

    Improved /join/ test. #1
    Modified tests to look for 404 instead of 200. So that fewer test users are created in and hence fewer will need to be cleaned up.
    
    Removed duplicate test for creating users.
    
    Added code that removes the user the tests create.

--- server/routes/join.spec.js
+++ server/routes/join.spec.js
@@ -25,34 +25,31 @@ describe('Join the site.', function() {
 
   it('Requires at least six characters in the secret', function(done) {
     request(
-      'http://localhost:1337/join/test/secret', 
+      'http://localhost:1337/join/test/secre', 
       function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
+        expect(response.statusCode).to.equal(404);
         done();
       }   
     );
   })
 
-  it('Lets new users join.', function(done) { 
-    request(
-      'http://localhost:1337/join/test/secret', 
-      function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      } 
-    );
-  });
-
-  it('Adds a user.', function(done) {
+  it('Add a user.', function(done) {
     request(
       'http://localhost:1337/join/tester/secret',
       function(error, response, body) {
         expect(response.statusCode).to.equal(200);
-        var file = fs.readFileSync('../users.json', 'utf8');
+        var file = fs.readFileSync('./server/users.json', 'utf8');
         var users = JSON.parse(file);
         expect(users.tester).to.exist;
         done();
       }
     );
+  });
+
+  after(function() {
+    var file = fs.readFileSync('./server/users.json', 'utf8');
+    var users = JSON.parse(file);
+    delete users.tester;
+    fs.writeFileSync('./server/users.json', JSON.stringify(users))
   });
 });
