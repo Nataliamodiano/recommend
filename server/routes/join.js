82f34395baac91fa4f35c3a9ed612fd018768a2e
ronperris <ronperris@gmail.com>
2015-10-18 16:41:48 -0700

    Add /join/ route for adding users. #1

--- server/routes/join.js
+++ server/routes/join.js
@@ -1,16 +1,34 @@
 var express = require('express');
 var app = express();
 var join = express.Router();
+var fs = require('fs');
 
 join.get('/:name/:secret', function(req, res){
   var name = req.params.name;
   var secret = req.params.secret;
 
   if(name.length > 3 && secret.length > 5) {
-    res.send('joined')
+    if(addUser(name, secret)) {
+      res.send('joined')
+    } else {
+      res.sendStatus(404);
+    }
   } else {
     res.sendStatus(404);
   }  
+
+  function addUser(name, secret) {
+    var file = fs.readFileSync(__dirname + '/../users.json', 'utf8');
+    var users = JSON.parse(file);
+
+    if(users[name] === undefined) {
+      users[name] = secret;       
+      fs.writeFileSync(__dirname + '/../users.json', JSON.stringify(users))
+      return true;
+    } else {
+      return false;
+    }
+  }
 });
 
 module.exports = join;
