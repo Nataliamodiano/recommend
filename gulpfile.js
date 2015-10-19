1ef51821239ceed20f5a1e23bbe14fd076ee3718
ronperris <ronperris@gmail.com>
2015-10-18 17:58:41 -0700

    Added build task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,16 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var nodemon = require('gulp-nodemon');
 
-gulp.task('default', function(){
+gulp.task('default', function() {
   nodemon({
     script: 'server/app.js',
     tasks: ['test']
   })
+})
+
+gulp.task('build', function() {
+  return gulp.src(['client/index.html'])
+    .pipe(gulp.dest('server/public'));
 })
 
 gulp.task('test', function() {
