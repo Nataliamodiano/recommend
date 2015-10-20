20878e5195595727e11c8d0f51905c81a859af2a
ronperris <ronperris@gmail.com>
2015-10-19 20:46:07 -0700

    Added test task to nodemon start.
    Moved the test task to the nodemon start event.

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,25 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var nodemon = require('gulp-nodemon');
 var concat = require('gulp-concat');
+var print = require('gulp-print');
+
+var libraries = [
+  'client/bower_components/angular/angular.min.js',
+  'client/bower_components/angular-route/angular-route.min.js'
+]
 
 var angular = [
   'client/app/**/*.module.js',
   'client/app/**/*.js'
 ];
 
+var app = 'server/app.js';
+
 gulp.task('default', function() {
   nodemon({
-    script: 'server/app.js',
-    tasks: ['test']
-  })
-})
+    script: app,
+  }).on('start', ['test']);
+});
 
 gulp.watch(['client/**/.js', 'client/*.html'], ['build']);
 
@@ -23,13 +30,18 @@ gulp.task('angular', function() {
     .pipe(gulp.dest('server/public'));
 })
 
-gulp.task('build', ['angular','concat'], function() {
+gulp.task('views', function() {
+  return gulp.src('client/**/*.html')
+    .pipe(gulp.dest('server/public'));
+})
+
+gulp.task('build', ['angular', 'views', 'concat'], function() {
   return gulp.src(['client/index.html'])
     .pipe(gulp.dest('server/public'));
 })
 
 gulp.task('concat', function() {
-  return gulp.src('client/bower_components/angular/angular.min.js') 
+  return gulp.src(libraries) 
     .pipe(concat('vendor.js'))
     .pipe(gulp.dest('server/public'))
 })
