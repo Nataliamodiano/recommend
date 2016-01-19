a9fccc4b16db9255cf7b3f23023ce53f3a72049e
natalia <nataliamodiano@gmail.com>
2016-01-19 10:00:41 -0800

    Modified git ignore and added public server files

---
+++ server/public/all.js
@@ -0,0 +1,25 @@
+angular.module('recommend', ['ngRoute']);
+
+angular.module('recommend')
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when("/join", {
+        templateUrl: "app/join/join.html",
+        controller: "joinController",
+        controllerAs: "join"
+      });
+  }])
+
+angular.module('recommend')
+   .controller('homeController', function() {
+     vm = this;
+     vm.title = "This is home";
+     vm.message = 'Welcome!';
+   });
+
+angular.module('recommend')
+   .controller('joinController', function() {
+     vm = this;
+     vm.title = "Join Us.";
+     vm.message = 'Thanks for joining!';
+   });
